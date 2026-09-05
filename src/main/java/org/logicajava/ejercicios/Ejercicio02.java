4ff58122ee0cceba5f0b193bb3760c8a24c3a33a
Javier Castillo <javier.developer.kt@gmail.com>
2026-09-04 22:54:29 -0600

    feat: add solution for exercise 2 - anagrama

---
+++ src/main/java/org/logicajava/ejercicios/Ejercicio02.java
@@ -0,0 +1,33 @@
+package org.logicajava.ejercicios;
+
+//¿QUE ES UN ANAGRAMA?
+
+/*
+ * Escribe una función que reciba dos palabras (String) y retorne
+ * verdadero o falso (Bool) según sean o no anagramas.
+ * - Un Anagrama consiste en formar una palabra reordenando TODAS
+ *   las letras de otra palabra inicial.
+ * - NO hace falta comprobar que ambas palabras existan.
+ * - Dos palabras exactamente iguales no son anagrama.
+ */
+
+public class Ejercicio02 {
+
+    public static void main(String[] args) {
+        Ejercicio02 ej = new Ejercicio02();
+        ej.anagrama("nader", "RedAN");
+    }
+
+    public void anagrama(String palabraUno, String palabraDos) {
+        String palabraUnoRevertida = new StringBuilder(palabraUno).reverse().toString().toUpperCase();
+        String palabraDosTratada = palabraDos.toUpperCase();
+        boolean resultado = false;
+
+        if (palabraUnoRevertida.equals(palabraDosTratada)) {
+            resultado = true;
+            System.out.println(resultado);
+        } else {
+            System.out.println(resultado);
+        }
+    }
+}
